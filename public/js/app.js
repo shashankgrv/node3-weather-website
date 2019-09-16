65765fb88c8d49b31d046023ce929bd3936240d3
Gaurav <SGaurav@in.imshealth.com>
2019-09-16 17:27:01 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("client side javascript file is loaded");
-
 const weatherForm = document.querySelector("form");
 const searchElement = document.querySelector("input");
 const messageOne = document.querySelector("#messageOne");
